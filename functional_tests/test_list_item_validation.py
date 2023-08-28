7af1b61a10adb8d0ad75331ce33780086483c8be
Alexander Voloshin <sannjka.v@gmail.com>
2023-08-28 16:12:56 +0300

    Use form in all views, back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -12,29 +12,34 @@ class ItemValidationTest(FunctionalTest):
 
         # The home page refreshes, and there is an error message waying
         # that list items cannot be blank
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
-            "You can't have an empty list item"
-        ))
+        self.wait_for(lambda:
+            self.browser.find_element(By.CSS_SELECTOR, '#id_text:invalid')
+        )
 
         # She tries again with some text for the item, which now works
         self.get_item_input_box().send_keys('Buy milk')
+        self.wait_for(lambda:
+            self.browser.find_element(By.CSS_SELECTOR, '#id_text:valid')
+        )
+
+        # And she can submit it successfully
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
         # Perversely, she now decides to submit  a second blank list item
         self.get_item_input_box().send_keys(Keys.ENTER)
 
-        # She receives a similar warning on the list page
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
-            "You can't have an empty list item"
-        ))
+        # Again, the browser will not comply
+        self.wait_for(lambda:
+            self.browser.find_element(By.CSS_SELECTOR, '#id_text:invalid')
+        )
 
         # And she can correct it by filling some text in
         self.get_item_input_box().send_keys('Make tea')
+        self.wait_for(lambda:
+            self.browser.find_element(By.CSS_SELECTOR, '#id_text:valid')
+        )
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
 
-        self.fail('write me!')
