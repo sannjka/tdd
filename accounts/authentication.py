72c50bb3d4da0e6924430e0c8bd8f12f1a8336b3
Alexander Voloshin <sannjka.v@gmail.com>
2023-08-30 18:25:03 +0300

    Custom passwordless auth backend + custom  user model

---
+++ accounts/authentication.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.backends import ModelBackend
+from accounts.models import User, Token
+
+class PasswordlessAuthenticationBackend(ModelBackend):
+    def authenticate(self, request=None, uid=None):
+        try:
+            token = Token.objects.get(uid=uid)
+            return User.objects.get(email=token.email)
+        except User.DoesNotExist:
+            return User.objects.create(email=token.email)
+        except Token.DoesNotExist:
+            return None
+
+    def get_user(self, email):
+        try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            return None
--- superlists/settings.py
+++ superlists/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,6 +53,15 @@ MIDDLEWARE = [
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
+]
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 ROOT_URLCONF = 'superlists.urls'
 
