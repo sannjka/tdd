6253fc7427f5e9b2bc1650b56afdbce8073dcedd
Alexander Voloshin <sannjka.v@gmail.com>
2023-09-01 12:10:54 +0300

    URL, placeholder view, and first-cut templates for my_lists

--- lists/urls.py
+++ lists/urls.py
@@ -21,4 +21,5 @@ from lists import views
 urlpatterns = [
     path('new', views.new_list, name='new_list'),
     path('<int:list_id>/', views.view_list, name='view_list'),
+    path('users/<email>/', views.my_lists, name='my_lists'),
 ]
